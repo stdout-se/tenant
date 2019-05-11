03cae4aa8057ffa0a703829b4f1c605268e35d98
Andreas Nilsson <github@version13.se>
2019-05-11 21:33:04 +0200

    Move to_json() / from_json() to bottom of classes

--- game_messages.py
+++ game_messages.py
@@ -34,6 +34,18 @@ class MessageLog:
     def __init__(self):
         self.messages = []
 
+    def add_message(self, message):
+        # Split the message if necessary, to multiple lines
+        new_msg_lines = textwrap.wrap(message.text, constants.message_width)
+
+        for line in new_msg_lines:
+            # If the buffer is full, remove the first line to make room for the new one
+            if len(self.messages) == constants.message_height:
+                del self.messages[0]
+
+            # Add the new line as a Message object, with the text and the color
+            self.messages.append(Message(line, message.color))
+
     def to_json(self):
         json_data = {
             'messages': [message.to_json() for message in self.messages]
@@ -51,15 +63,3 @@ class MessageLog:
             message_log.add_message(Message.from_json(message_json))
 
         return message_log
-
-    def add_message(self, message):
-        # Split the message if necessary, to multiple lines
-        new_msg_lines = textwrap.wrap(message.text, constants.message_width)
-
-        for line in new_msg_lines:
-            # If the buffer is full, remove the first line to make room for the new one
-            if len(self.messages) == constants.message_height:
-                del self.messages[0]
-
-            # Add the new line as a Message object, with the text and the color
-            self.messages.append(Message(line, message.color))
